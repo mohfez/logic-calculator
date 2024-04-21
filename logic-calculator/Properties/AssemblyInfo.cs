88d4c475b4267d72467c56f65d83d0a8b31a0595
unknown <unknown@unknown.com>
2024-04-21 20:15:21 +0100

    Added files

--- logic-calculator/Properties/AssemblyInfo.cs
+++ logic-calculator/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("logic-calculator")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyCopyright("Copyright © mohfez 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
